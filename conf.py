be1afcda74cb61209199c123d7bc8f6774d74aef
Bülent Özden <bulentozden2007@gmail.com>
2023-11-14 07:43:57 +0300

    Move stuff to conf

---
+++ conf.py
@@ -0,0 +1,11 @@
+""" cv-tbox Split Creator - Configuration File """
+
+# Program parameters
+
+
+# If true, report all on different lines, else show only generated
+VERBOSE: bool = False
+# If true, fail if source is not found, else skip it
+FAIL_ON_NOT_FOUND: bool = True
+# If true, regenerate the splits even if they exist,
+FORCE_CREATE: bool = False
